0f428ccd99784b60fdb5ef44ff0786f08fe1ddce
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-20 12:38:36 +0000

    add test for stpe 3

---
+++ tests/use_private_route.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import PrivateRoute from './components/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
+import Settings from "../components/Settings";
+import Login from "../components/Login";
+
+
+describe('PrivateRoute', () => {
+    test('unauthenticated users cannot access settings page', async () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/settings']}>
+          <PrivateRoute currentUser={null} path="/settings" component={Settings} />
+        </MemoryRouter>
+      );
+      expect(wrapper.find(Settings)).toHaveLength(0);
+    });
+});
+
+
+describe('PrivateRoute', () => {
+    test('unauthenticated users cannot access settings page', async () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/']}>
+          <PrivateRoute currentUser={null} path="/settings" component={Settings} />
+        </MemoryRouter>
+      );
+      expect(wrapper.find(Settings)).toHaveLength(0);
+    });
+});
+
+
+
+describe('PrivateRoute', () => {
+    test('unauthenticated users cannot access settings page', async () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/']}>
+          <PrivateRoute currentUser={null} path="/login" component={Login} />
+        </MemoryRouter>
+      );
+      expect(wrapper.find(Settings)).not.toHaveLength(0);
+    });
+});
