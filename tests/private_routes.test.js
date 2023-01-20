b3c544c2bf698b630733981239ca272781c71f39
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-20 10:42:28 +0000

    add memor router

--- tests/private_routes.test.js
+++ tests/private_routes.test.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import PrivateRoute from './components/PrivateRoute';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('PrivateRoute', () => {
   test('renders the component if the user is authenticated', () => {
     const Component = () => <div>Protected Content</div>;
     const currentUser = { isAuthenticated: true };
-    render(<PrivateRoute currentUser={currentUser} component={Component} />);
+    render(
+        <MemoryRouter initialEntries={['/']}>
+    <PrivateRoute currentUser={currentUser} component={Component} />
+    </MemoryRouter>
+    );
     expect(screen.getByText('Protected Content')).toBeInTheDocument();
   });
 
   test('does not render the component if the user is not authenticated', () => {
     const Component = () => <div>Protected Content</div>;
     const currentUser = null;
-    render(<PrivateRoute currentUser={currentUser} component={Component} />);
+    render(
+        <MemoryRouter initialEntries={['/']}>
+    <PrivateRoute currentUser={currentUser} component={Component} />
+    </MemoryRouter>
+    );
     expect(screen.queryByText('Protected Content')).toBeNull();
   });
 });
