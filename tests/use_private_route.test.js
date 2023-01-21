9e7099dc3ea522039d7bb648586312310cc3300a
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-21 07:40:40 +0000

    update test

--- tests/use_private_route.test.js
+++ tests/use_private_route.test.js
@@ -3,8 +3,9 @@ import { mount } from 'enzyme';
 import PrivateRoute from './components/PrivateRoute';
 import { MemoryRouter } from 'react-router-dom';
 import Settings from "./components/Settings";
+import Editor from "./components/Editor";
 
-describe('PrivateRoute', () => {
+describe('PrivateRoute configuration for settings page', () => {
     test('unauthenticated users cannot access settings page', async () => {
       const wrapper = mount(
         <MemoryRouter initialEntries={['/settings']}>
@@ -13,29 +14,34 @@ describe('PrivateRoute', () => {
       );
       expect(wrapper.find(Settings)).toHaveLength(0);
     });
-});
 
-
-describe('PrivateRoute', () => {
-    test('unauthenticated users cannot access settings page', async () => {
-      const wrapper = mount(
-        <MemoryRouter initialEntries={['/']}>
-          <PrivateRoute currentUser={null} path="/settings" component={Settings} />
-        </MemoryRouter>
-      );
-      expect(wrapper.find(Settings)).toHaveLength(0);
-    });
-});
-
-
-
-describe('PrivateRoute', () => {
     test('authenticated users can access settings page', async () => {
       const wrapper = mount(
-        <MemoryRouter initialEntries={['/']}>
+        <MemoryRouter initialEntries={['/settings']}>
           <PrivateRoute currentUser={{username:"test", email:"test@mail.com"}} path="/settings" component={Settings} />
         </MemoryRouter>
       );
       expect(wrapper.find(Settings)).toHaveLength(1);
+    });
+});
+
+
+describe('PrivateRoute configuration for settings page', () => {
+    test('unauthenticated users cannot access editor page', async () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/editor']}>
+          <PrivateRoute currentUser={null} path="/editor" component={Editor} />
+        </MemoryRouter>
+      );
+      expect(wrapper.find(Editor)).toHaveLength(0);
+    });
+
+    test('authenticated users can access editor page', async () => {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/editor']}>
+          <PrivateRoute currentUser={{username:"test", email:"test@mail.com"}} path="/editor" component={Editor} />
+        </MemoryRouter>
+      );
+      expect(wrapper.find(Editor)).toHaveLength(1);
     });
 });
